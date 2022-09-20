4ce66ede2e2eea6b2718a60757da98f90a065fcf
Shelton Simbi <shltnsimbi@gmail.com>
2022-09-20 05:44:30 -0800

    Modified the card appearance

--- src/app/property/property-card/property-card.component.ts
+++ src/app/property/property-card/property-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-card',
@@ -7,12 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyCardComponent implements OnInit {
 
-  Property: any = {
-    'Id': 1,
-    'Name': 'Shelton range',
-    'Type': 'House',
-    'Price': 12000
-  }
+  @Input() property: any;
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/property/property-list/property-list.component.ts
+++ src/app/property/property-list/property-list.component.ts
@@ -7,6 +7,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
+  properties: Array<any> = [
+    {
+      'Id': 1,
+      'Name': 'Shelton range',
+      'Type': 'House',
+      'Price': 12000
+    },
+    {
+      'Id': 2,
+      'Name': 'Philadelphia',
+      'Type': 'House',
+      'Price': 124876
+    },
+    {
+      'Id': 3,
+      'Name': 'Bullaster',
+      'Type': 'House',
+      'Price': 78256
+    },
+    {
+      'Id': 4,
+      'Name': 'CauseWay',
+      'Type': 'House',
+      'Price': 19829
+    },
+    {
+      'Id': 5,
+      'Name': 'Pilot Nerja',
+      'Type': 'House',
+      'Price': 1566
+    }
+  ]
   constructor() { }
 
   ngOnInit(): void {
